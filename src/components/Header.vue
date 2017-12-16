1d77a11efc1bec2977b932ca684ca81eb4c6e3b5
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-12-16 21:54:26 +0000

    add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <h2>The Resettlement Service</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  flex-grow: 0;
+  flex-shrink: 1;
+  flex-basis: 60px;
+  background-color: #336699;
+  color: #fff;
+}
+h2 {
+  text-align: center;
+  font-weight: 400;
+}
+</style>
