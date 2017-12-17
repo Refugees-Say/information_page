429a1c6a1e63e46c95e5cd49e7dce3b366d87e9e
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-12-17 22:58:25 +0000

    add header and footer to app file

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Header/>
     <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 export default {
   name: 'app',
+  components: { Header, Footer },
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Open Sans", "sans-serif";
+  font-weight: 300;
+  display: flex;
+  flex-flow: column;
+  min-height: 100vh;
+  background-image: url(./assets/bg_photo.jpg);
+  background-size: cover;
+  margin: 0px;
+  padding: 0px;
 }
 </style>
