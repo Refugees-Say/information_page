3c6178d717ba127af6cb3dc6e6270461da880ca6
Md-Almasri <mohieddin.almasri@hotmail.com>
2017-12-21 16:49:40 +0000

    apply scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,16 +11,16 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 .header {
   flex-grow: 0;
   flex-shrink: 1;
   flex-basis: 60px;
   background-color: #336699;
   color: #fff;
-}
-h2 {
-  text-align: center;
-  font-weight: 400;
+  h2 {
+    text-align: center;
+    font-weight: 400;
+  }
 }
 </style>
